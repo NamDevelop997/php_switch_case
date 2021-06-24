a7aa0f83771269c7d485cb832fe3defb0012bb08
Namdevelop997 <namdevelop997@gmail.com>
2021-06-25 03:04:18 +0700

    demo use switch case

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$number = 5;
+switch ($number) {
+    case 3:
+        echo "value = 3";
+        break;
+    case 5:
+        echo "value = 5";
+        break;
+    default:
+        echo "error";
+        break;
+}
